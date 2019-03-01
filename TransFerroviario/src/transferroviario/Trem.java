abb55a62a4dc65ac64c8f68a2e57ea42eb88316b
Aluno CESJF <alunoces@labcesac.net>
2019-03-01 20:11:40 -0300

    classes Composicao,trem, vagao

---
+++ TransFerroviario/src/transferroviario/Trem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transferroviario;
+
+/**
+ *
+ * @author alunoces
+ */
+public class Trem {
+    private String prefixoTrem ;
+    private String dataFormacao;
+
+    public String getPrefixoTrem() {
+        return prefixoTrem;
+    }
+
+    public void setPrefixo_Trem(String prefixoTrem) {
+        this.prefixoTrem = prefixoTrem;
+    }
+
+    public String getDataFormacao() {
+        return dataFormacao;
+    }
+
+    public void setData_Formacao(String dataFormacao) {
+        this.dataFormacao = dataFormacao;
+    }
+}
