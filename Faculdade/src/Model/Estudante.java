1edb7d570c809be7cafe77947d769f0564f0198f
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-03-22 19:32:19 -0300

    classe Estudante

---
+++ Faculdade/src/Model/Estudante.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author alunoces
+ */
+public class Estudante {
+    public String numeroMatricula;
+    public String nome;
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
