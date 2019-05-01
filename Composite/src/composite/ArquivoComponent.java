2b1d8cc02d966b32e3a76a00ac30d32d6c94f21f
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-04-30 22:20:57 -0300

    ArquivoComposite

--- Composite/src/composite/ArquivoComponent.java
+++ Composite/src/composite/ArquivoComponent.java
@@ -15,5 +15,18 @@ public abstract class ArquivoComponent {
     public void printNomeDoArquivo(){
         System.out.println(this.nomeDoArquivo);
     }
+    public String getNomeDoArquivo(){
+        return this.nomeDoArquivo;
+    }
+    public void adicionar(ArquivoComponent novoArquivo ) throws Exception{
+        throw new Exception("Não pode inserir arquivos em: " + this.nomeDoArquivo + " - Não é uma pasta");
+    }
+    public void remover(String nomeDoArquivo) throws Exception{
+        throw new Exception("Não pode remover arquivos em: " + this.nomeDoArquivo + " -Não é uma pasta");
+    }
+    public ArquivoComponent getArquivo(String nomeDoArquivo) throws Exception{
+        throw new Exception("Não pode pesquisar arquivos em: " + this.nomeDoArquivo + " - Não é uma pasta");
+    }
+
     
 }
