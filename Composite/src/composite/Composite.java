5d675613a5eaeab944042e88d3b10743506b2c47
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-05-07 21:57:02 -0300

    Principal composite

--- Composite/src/composite/Composite.java
+++ Composite/src/composite/Composite.java
@@ -14,8 +14,34 @@ public class Composite {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   public static void main(String[] args) {
+    ArquivoComponent minhaPasta = new ArquivoComposite("Minha Pasta/");
+    ArquivoComponent meuVideo = new ArquivoVideo("meu video.avi");
+    ArquivoComponent meuOutroVideo = new ArquivoVideo("serieS01E01.mkv");
+        try {
+            meuVideo.adicionar(meuOutroVideo);
+            }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
     }
+        try {
+            minhaPasta.adicionar(meuVideo);
+            minhaPasta.adicionar(meuOutroVideo);
+            minhaPasta.printNomeDoArquivo();
+            }
+        catch (Exception e) {
+             System.out.println(e.getMessage());
+    }
+        try {
+            System.out.println("\nPesquisando arquivos:");
+            minhaPasta.getArquivo(meuVideo.getNomeDoArquivo()).printNomeDoArquivo();
+            System.out.println("\nRemover arquivos");
+            minhaPasta.remover(meuVideo.getNomeDoArquivo());
+            minhaPasta.printNomeDoArquivo();
+            } 
+        catch (Exception e) {
+             e.printStackTrace();
+            }
+}
     
 }
